b67d704ce518451cba1f877e34c86741422cb3b0
王銘熙 <karta571110@gmail.com>
2019-11-08 14:58:34 +0800

    add FaceWord

--- app/src/main/java/com/example/appclass_hw10_invoice/Main3Activity.java
+++ app/src/main/java/com/example/appclass_hw10_invoice/Main3Activity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class Main3Activity extends AppCompatActivity {
 
     TextView textPrize;
+    TextView textText;
     TextView textFace;
     Button btnBack;
     Button btnMonth;
@@ -23,6 +24,7 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
         textPrize = findViewById(R.id.TextPrize);
         textFace = findViewById(R.id.TextFace);
+        textText = findViewById(R.id.Texttext);
         btnBack = findViewById(R.id.BtnBack2);
         btnMonth = findViewById(R.id.BtnMonth);
         textPrize.setText("");
@@ -35,8 +37,10 @@ public class Main3Activity extends AppCompatActivity {
 
         if (editNubmer.equals(pNumber[0])) {
             textPrize.setText("獎金為：一千萬！");
+            textText.setText("Σ(ﾟДﾟ；≡；ﾟдﾟ)");
         } else if (editNubmer.equals(pNumber[1])) {
             textPrize.setText("獎金為：兩百萬！");
+            textText.setText("｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡");
         } else {
 
             for (int j = 2; j <= 4; j++) {
@@ -45,21 +49,27 @@ public class Main3Activity extends AppCompatActivity {
                         switch (i) {
                             case 0:
                                 textPrize.setText("獎金為：二十萬！");
+                                textText.setText("ε٩(๑> ₃ <)۶з");
                                 break;
                             case 1:
                                 textPrize.setText("獎金為：四萬！");
+                                textText.setText("(๑´ㅂ`๑)");
                                 break;
                             case 2:
                                 textPrize.setText("獎金為：一萬！");
+                                textText.setText("✧◝(⁰▿⁰)◜✧");
                                 break;
                             case 3:
                                 textPrize.setText("獎金為：四千！");
+                                textText.setText("(๑•̀ㅂ•́)و✧");
                                 break;
                             case 4:
                                 textPrize.setText("獎金為：一千！");
+                                textText.setText("ヽ(●´ε｀●)ノ");
                                 break;
                             case 5:
                                 textPrize.setText("獎金為：兩百！");
+                                textText.setText("(•ㅂ•)/");
                                 break;
                             default:
                                 break;
@@ -75,6 +85,7 @@ public class Main3Activity extends AppCompatActivity {
                 for (int k = 5; k < pNumber.length; k++) {
                     if (editNubmer.substring(5, 8).equals(pNumber[k].substring(0, 3))) {
                         textPrize.setText("獎金為：兩百！");
+                        textText.setText("(•ㅂ•)/");
                         break;
                     }
                 }
@@ -82,6 +93,7 @@ public class Main3Activity extends AppCompatActivity {
 
             if (textPrize.getText().equals("")) {
                 textPrize.setText("很可惜 並沒有中獎");
+                textText.setText("-`д´-");
             }
         }
 
